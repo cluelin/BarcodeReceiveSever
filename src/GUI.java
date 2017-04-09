458038d31d00b27526f4959012d0a900ead032ac
WTG_SSD\cluelin <cluelin@gmail.com>
2017-04-09 16:37:04 -0400

    Save .txt feature Bug fix (freezing)

--- src/GUI.java
+++ src/GUI.java
@@ -1,54 +1,77 @@
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
 
-public class GUI extends JFrame implements ActionListener{
+public class GUI extends JFrame implements ActionListener {
+
+	static GUI GUIinstance = null;
 
 	JPanel jPanel;
 	JTextArea barcodeTextArea;
 	JButton barcodeAreaClearButton;
-	
-	public GUI(){
+
+	private GUI() {
 		super("BarCode Receiver");
-				
+
 		barcodeTextArea = new JTextArea();
 		barcodeTextArea.setEditable(false);
-		barcodeTextArea.setFont(barcodeTextArea.getFont().deriveFont(20f)); 
-		
+		barcodeTextArea.setFont(barcodeTextArea.getFont().deriveFont(20f));
+
 		JScrollPane scrollPane = new JScrollPane(barcodeTextArea);
 		add(scrollPane);
-		
-		
-		
+
 		barcodeAreaClearButton = new JButton("지우기");
 		barcodeAreaClearButton.addActionListener(this);
-		
+
 		jPanel = new JPanel();
 		jPanel.add(barcodeAreaClearButton);
-		
+
 		add(jPanel, BorderLayout.SOUTH);
+
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(500, 400);
+		this.setVisible(true);
+
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
 	}
 
-	//set barcode Area
-	public void setBarcodeArea(String barcodeValue){
-		
-		barcodeTextArea.setText(barcodeTextArea.getText() + "\n" + barcodeValue);
+	public static GUI getInstance() {
+
+		if (GUIinstance == null) {
+			GUIinstance = new GUI();
+		}
+
+		return GUIinstance;
+
+	}
+
+	// set barcode Area
+	public void setBarcodeArea(String barcodeValue) {
+
+		// barcodeTextArea.setText(barcodeTextArea.getText() + "\n" +
+		// barcodeValue);
+		barcodeTextArea.append(barcodeValue + "\n");
+
+		// File file = new File();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		
-		//현재 설정된 바코드 값들의 초기화. 
-		if(event.getSource() == barcodeAreaClearButton){
+
+		// 현재 설정된 바코드 값들의 초기화.
+		if (event.getSource() == barcodeAreaClearButton) {
 			barcodeTextArea.setText("");
 		}
-		
+
 	}
 }
--- src/ReceiverServer.java
+++ src/ReceiverServer.java
@@ -1,20 +1,27 @@
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.ParseException;
 
+import javax.print.attribute.standard.Finishings;
 import javax.swing.JFrame;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class ReceiverServer implements Runnable {
 
 	public static final int ServerPort = 9999;
-	public static final String ServerIP = "192.168.0.63";
-	private static GUI gui;
+	
+	JSONParser parser = new JSONParser();
+	String fileName;
 
 	@Override
 	public void run() {
@@ -22,41 +29,58 @@ public class ReceiverServer implements Runnable {
 		try {
 			System.out.println("S: Connecting...");
 			ServerSocket serverSocket = new ServerSocket(ServerPort);
+			
+			GUI.getInstance();
 
 			while (true) {
-				Socket client = serverSocket.accept();
 				System.out.println("S: Receiving...");
+				Socket client = serverSocket.accept();
+
 				try {
 					BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-					
-					String str = "";
-					
-					while(true){
-						
-						 str = in.readLine();
-						
-						if(str.compareTo("null") == 0)
-							break;
-						
-						//콘솔로 출력. 
-						System.out.println("S: Received: '" + str + "'");
-						gui.setBarcodeArea(str);
-												
-					}
-					
-					//GUI에 바코드값 출력
-					
-										
-					
 
-					
-					//클라이언트로 다시 출력해주는 기능. 
-//					PrintWriter out = new PrintWriter(
-//							new BufferedWriter(new OutputStreamWriter(client.getOutputStream())), true);
-//					out.println("Server Received " + str);
+					String str = "";
+					String barcodeVal = "";
+
+					while (true) {
+
+						str = in.readLine();
+
+						// 입력 끝나면 null.
+						if (str == null)
+							break;
+
+						// 들어온 입력이 SO일때 처리.
+						try {
+							JSONObject obj = (JSONObject) parser.parse(str);
+
+							fileName = obj.get("soNumber").toString();
+
+							saveBarcode(fileName, barcodeVal);
+
+							// SO일경우에는 뒤에꺼 Rx, Tx진행하지말고 걍 넘겨.
+							continue;
+
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+
+						// 콘솔로 출력.
+						System.out.println("S: Received: '" + str + "'");
+						GUI.getInstance().setBarcodeArea(str);
+						barcodeVal = barcodeVal + str + System.getProperty("line.separator");
+
+					}
+
+					// 클라이언트로 다시 출력해주는 기능.
+					// PrintWriter out = new PrintWriter(
+					// new BufferedWriter(new
+					// OutputStreamWriter(client.getOutputStream())), true);
+					// out.println("Server Received " + str);
 				} catch (Exception e) {
 					System.out.println("S: Error");
 					e.printStackTrace();
+
 				} finally {
 					client.close();
 					System.out.println("S: Done.");
@@ -68,18 +92,29 @@ public class ReceiverServer implements Runnable {
 		}
 	}
 
+	private void saveBarcode(String soNumber, String barcodeVal) {
+
+		File file = new File("./Barcode\\" + soNumber + ".txt");
+
+		try {
+			file.createNewFile();
+			PrintWriter writer = new PrintWriter(file);
+
+			writer.print(barcodeVal);
+			writer.flush();
+
+			writer.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
 	public static void main(String[] args) {
 
 		Thread desktopServerThread = new Thread(new ReceiverServer());
 		desktopServerThread.start();
-
-		gui = new GUI();
-		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		gui.setSize(500, 400);
-		gui.setVisible(true);
-
-		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-		gui.setLocation(dim.width / 2 - gui.getSize().width / 2, dim.height / 2 - gui.getSize().height / 2);
 
 	}
 }
